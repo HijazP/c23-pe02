c0e6b8fe506172426bfd6b9b40cd5833eb6b0d18
rifqimn <rifqimn48@gmail.com>
2023-05-28 17:25:35 +0700

    Membuat Preferences Datastore (belum selesai)

---
+++ app/src/main/java/com/amati/amatiapp/database/UserPreferencesDatastore.kt
@@ -0,0 +1,39 @@
+package com.amati.amatiapp.database
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+class UserPreferencesDatastore(context: Context) {
+    private val Context.dataStore by preferencesDataStore("user_pref")
+    private val dataStore = context.dataStore
+
+    companion object {
+        private val USER_NAME = stringPreferencesKey("user_name")
+        private val USER_ID = stringPreferencesKey("user_id")
+        private val USER_TOKEN = stringPreferencesKey("user_token")
+    }
+
+    suspend fun storeUser(name: String, id: String, token: String) {
+        dataStore.edit { pref ->
+            pref[USER_NAME] = name
+            pref[USER_ID] = id
+            pref[USER_TOKEN] = token
+        }
+    }
+
+    suspend fun logout() {
+        dataStore.edit { pref ->
+            pref[USER_NAME] = ""
+            pref[USER_ID] = ""
+            pref[USER_TOKEN] = ""
+        }
+    }
+
+//  fun getUser(): Flow< >{
+//      return dataStore.data.map{ pref ->
+//      }
+//   }
+
+}
