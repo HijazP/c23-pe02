dc03cff8080a290f40030881a58fa88ad0a7ea27
Rafza Ray Firdaus <xxrafzaxx@gmail.com>
2023-06-05 16:17:35 +0700

    Menambah api course-to-problem
    Terdapat 2 api dalam satu script ini, yakni: - Course-to-course dan - Course-to-problem

--- app.py
+++ app.py
@@ -2,22 +2,23 @@ from typing import List
 import pandas as pd
 import pickle
 from fastapi import FastAPI
+import sklearn 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
-with open('.\load_model\cos_sim.pkl', 'rb') as file:
+with open('./load_model/cos_sim.pkl', 'rb') as file:
     cos_sim = pickle.load(file)
 
-with open('.\load_model\cosine_similarity.pkl', 'rb') as file:
+with open('./load_model/cosine_similarity.pkl', 'rb') as file:
     cosine_similarity = pickle.load(file)
 
-with open('.\load_model\course.pkl', 'rb') as file:
+with open('./load_model/course.pkl', 'rb') as file:
     course = pickle.load(file)
 
-with open('.\load_model\tfidf_matrix.pkl', 'rb') as file:
+with open('./load_model/tfidf_matrix.pkl', 'rb') as file:
     tfidf_matrix = pickle.load(file)
 
 # Generate recommendations
@@ -45,10 +46,69 @@ def recommendations(name, model, dataset, cos_sim):
     return recommended_course
 
 # Memuat model dari file .h5
-model = load_model('.\load_model\model.h5')
+model = load_model('./load_model/model.h5')
 
 @app.get("/recommendations/{name}")
 def get_recommendations(name: str):
     title = name
     recommended_course = recommendations(title, model, course, cos_sim)
     return {"rekomendasi":recommended_course}
+
+
+###############################
+
+
+with open('./load_model/course_clean.pkl', 'rb') as file:
+    course_clean = pickle.load(file)
+
+with open('./load_model/prob_clean.pkl', 'rb') as file:
+    prob_clean = pickle.load(file)
+
+
+def find_nearest_similarity(dataset1, dataset2):
+    # Extract text and index from DataFrame
+    text1 = dataset1['summary'].tolist()
+    text2 = dataset2['problem'].tolist()
+    index1 = dataset1.index.tolist()
+    index2 = dataset2.index.tolist()
+
+    # Vectorize the datasets using TF-IDF
+    vectorizer = TfidfVectorizer()
+
+    vectors1 = vectorizer.fit_transform(text1)
+    vectors2 = vectorizer.transform(text2)
+
+    # Calculate cosine similarity
+    similarity_matrix = cosine_similarity(vectors1, vectors2)
+
+    # Find the nearest similarity for each row in dataset1
+    nearest_similarities = {}
+    nearest_indices = {}
+
+    for i in range(len(dataset1)):
+        max_similarity = -1
+        max_index = -1
+
+        for j in range(len(dataset2)):
+            if similarity_matrix[i, j] > max_similarity:
+                max_similarity = similarity_matrix[i, j]
+                max_index = index2[j]
+
+        nearest_similarities[index1[i]] = max_similarity
+        nearest_indices[index1[i]] = max_index
+
+    return nearest_indices, nearest_similarities
+
+def get_top_3_similar_indices(index1, similarity_matrix):
+    similarities = similarity_matrix[index1]
+    sorted_indices = similarities.argsort()[::-1][:3]
+
+    return sorted_indices.tolist()
+
+
+@app.get("/predict/{input}")
+def get_recommendations(input: str):
+
+    nearest_indices, nearest_similarities = find_nearest_similarity(course_clean, prob_clean)
+
+    return {"Nearest similarity pair for [{}]: [{}], Similarity: {}".format([input], nearest_indices[input], nearest_similarities[input])}
