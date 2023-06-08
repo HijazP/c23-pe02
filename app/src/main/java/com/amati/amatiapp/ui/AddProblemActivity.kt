4aa6439f0332964e5c411b65223643e9395a1ff2
Hanif Ali Ramdani <95014125+pulungrafi@users.noreply.github.com>
2023-06-08 15:10:26 +0700

    Add Problem
    masih ada kendala omw kelarin

---
+++ app/src/main/java/com/amati/amatiapp/ui/AddProblemActivity.kt
@@ -0,0 +1,47 @@
+package com.amati.amatiapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.amati.amatiapp.R
+import com.amati.amatiapp.databinding.ActivityAddProblemBinding
+import com.amati.amatiapp.viewmodel.addProblemViewModel
+
+class AddProblemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddProblemBinding
+    private val addProblemViewModel: addProblemViewModel by viewModels{}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddProblemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addProblemAct()
+    }
+
+    private fun addProblemAct(){
+        binding.btnProblem.setOnClickListener{
+            binding.apply {
+                inputProblem.clearFocus()
+                inputDesc.clearFocus()
+
+                val problem = inputProblem.text.toString()
+                val generalDesc = inputDesc.text.toString()
+
+                when{
+                    problem.isEmpty() -> {
+                        inputProblem.error = getString(R.string.tittle_req)
+                    }
+                    generalDesc.isEmpty() -> {
+                        inputDesc.error = getString(R.string.desc_req)
+                    }
+                    else -> {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent) }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/amati/amatiapp/viewmodel/addProblemViewModel.kt
@@ -0,0 +1,5 @@
+package com.amati.amatiapp.viewmodel
+
+class addProblemViewModel {
+
+}
